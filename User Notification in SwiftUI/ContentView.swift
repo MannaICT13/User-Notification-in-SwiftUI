4d0157dd4a6cd88cee356e77c214ea5a5f566adc
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-01-09 02:05:30 +0600

    Initial Commit

---
+++ User Notification in SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  User Notification in SwiftUI
+//
+//  Created by Md Khaled Hasan Manna on 9/1/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
